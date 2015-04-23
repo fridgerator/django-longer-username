a5ba4d221f600be4c2b7494f48cd54a39074d0e0
Andrew Chen <ayc@yelp.com>
2015-04-23 14:56:35 -0700

    add error_message to username field

--- longerusername/migrations/0001_initial.py
+++ longerusername/migrations/0001_initial.py
@@ -65,13 +65,7 @@ class Migration(migrations.Migration):
             app_label='auth',
             model_name='user',
             name='username',
-            field=models.CharField(
-                help_text='Required, 255 characters or fewer. Only letters, numbers, and @, ., +, -, or _ characters.',
-                unique=True,
-                max_length=255,
-                verbose_name='username',
-                validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username.', 'invalid')]
-            ),
+            field=models.CharField(error_messages={'unique': 'A user with that username already exists.'}, max_length=255, validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username. This value may contain only letters, numbers and @/./+/-/_ characters.', 'invalid')], help_text='Required, 255 characters or fewer. Only letters, numbers, and @, ., +, -, or _ characters.', unique=True, verbose_name='username'),
             preserve_default=True,
         ),
     ]
